9520b606c956e08be1fa7b14e8bfcfe428a8e4bb
Danuhu <dan.pro8k@gmail.com>
2025-06-19 09:27:28 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,25 +1,26 @@
 
-import os
-import telebot
 from flask import Flask, request
+import telebot
+import os
 
 API_TOKEN = os.getenv("API_TOKEN")
-if not API_TOKEN:
-    raise ValueError("API_TOKEN não definido. Verifique as variáveis de ambiente no Render.")
-
 bot = telebot.TeleBot(API_TOKEN)
-
-bot.remove_webhook()  # Linha que corrige o erro 409
 
 app = Flask(__name__)
 
-@bot.message_handler(commands=['start'])
-def send_welcome(message):
-    bot.reply_to(message, "Olá! Eu sou um bot do Telegram.")
+@app.route('/' + API_TOKEN, methods=['POST'])
+def getMessage():
+    json_str = request.get_data().decode('UTF-8')
+    update = telebot.types.Update.de_json(json_str)
+    bot.process_new_updates([update])
+    return "!", 200
 
-@bot.message_handler(func=lambda message: True)
-def echo_all(message):
-    bot.reply_to(message, message.text)
+@app.route("/", methods=['GET'])
+def webhook():
+    bot.remove_webhook()
+    webhook_url = os.getenv("RENDER_EXTERNAL_URL") + API_TOKEN
+    bot.set_webhook(url=webhook_url)
+    return "Webhook set!", 200
 
 if __name__ == "__main__":
-    bot.polling()
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
